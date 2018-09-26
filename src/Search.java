5c5fb55c8d5e563719fc799ed565b5e1630c3d14
Joe Chen <joejinghongchen@familysearch.org>
2018-09-25 20:38:34 -0600

    finished implementing main class

--- src/Search.java
+++ src/Search.java
@@ -6,10 +6,7 @@ import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.stream.Collectors;
+import java.util.*;
 import java.util.stream.Stream;
 
 import org.jsoup.Jsoup;
@@ -23,6 +20,8 @@ public class Search {
 
   private ArrayList<String> links = new ArrayList<>();
   private HashMap<String, LinkedList<Node>> words = new HashMap<>();
+  private ArrayList<String> keywordIndex = null;
+  private ArrayList<Map.Entry<String, Integer>> wordFrequencyIndex = null;
 
   private class Node {
     boolean isCapitalized;
@@ -66,8 +65,8 @@ public class Search {
     String[] splits = doc.body().text().split("\\s+");
     int positionCount = 0;
     for (String s : splits) {
-      String keyword = s.replaceAll("\"|\\p{Punct}+$", "");
-      if (!keyword.isEmpty()) {
+      String keyword = s.replaceAll("^\\p{Punct}+|\\p{Punct}+$", "");
+      if (!keyword.isEmpty() && keyword.matches("[a-zA-Z].*")) {
         if (words.containsKey(keyword.toLowerCase())) {
           words.get(keyword.toLowerCase()).add(new Node(keyword.matches("[A-Z].*"), positionCount));
         } else {
@@ -80,21 +79,82 @@ public class Search {
     }
   }
 
-  public void printResult() {
+  public void sortKeywordIndex() {
+    keywordIndex = new ArrayList<>(words.keySet());
+    Collections.sort(keywordIndex);
+  }
+
+  public void sortWordFrequencyIndex() {
+    wordFrequencyIndex = new ArrayList<>();
+    for (String key : words.keySet()) {
+      wordFrequencyIndex.add(new AbstractMap.SimpleEntry(key, words.get(key).size()));
+    }
+    Collections.sort(wordFrequencyIndex, (m1, m2) -> {
+      if (m1.getValue() == m2.getValue()) {
+        return 0;
+      } else if (m1.getValue() < m2.getValue()) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+
+  private String printNodeInfo(LinkedList<Node> nodeList) {
+    StringBuilder sb = new StringBuilder();
+    sb.append("[");
+    for (Node n : nodeList) {
+      sb.append("(");
+      sb.append(n.position);
+      sb.append(",");
+      sb.append(n.isCapitalized ? "Y" : "N");
+      sb.append("),");
+    }
+    sb.setLength(Math.max(sb.length() - 1, 0));
+    sb.append("]");
+    return sb.toString();
+  }
+
+  public void printResult(int printNumber) {
+    int count;
     System.out.println("\nLinks: " + links.size());
     for (String link : links) {
       System.out.println(link);
     }
-    System.out.println("\nword count: ");
-    for (String keyword : words.keySet()) {
-      System.out.printf("%s : %s \n", keyword, words.get(keyword).size());
+    System.out.printf("\nTop %d words sorted in alphabetical order, format:[(position, isCapitalized)] :\n", printNumber);
+    count = 0;
+    for (String keyword : keywordIndex) {
+      count++;
+      if (count > printNumber) {
+        break;
+      }
+      System.out.printf("%s : %s \n", keyword, printNodeInfo(words.get(keyword)));
+    }
+    System.out.printf("\nTop %d words sortecd in frequency order: \n", printNumber);
+    count = 0;
+    for (Map.Entry entry : wordFrequencyIndex) {
+      count++;
+      if (count > printNumber) {
+        break;
+      }
+      System.out.printf("%s : %s \n", entry.getKey(), words.get(entry.getKey()).size());
     }
   }
 
   public static void main(String[] args){
-    if (args.length < 1 || args[0].equals("--help")) {
-      System.out.println("Usage: 'java Search URI', the URI parameter should be a URL or file name to search.");
+    if (args.length < 2 || args[0].equals("--help")) {
+      System.out.println("Usage: 'java Search URL print',"
+        + " the URL 'parameter' should be a link to search,"
+        + " and the 'print' parameter should be top number of list to print.");
       return;
+    }
+    int printNumber = 0;
+    try {
+      printNumber = Integer.parseInt(args[1]);
+    }
+    catch (NumberFormatException e) {
+      System.out.println("Print Number parameter Error: " + e.getMessage());
+      System.exit(1);
     }
     Search newSearch = new Search();
     String html = "";
@@ -103,9 +163,12 @@ public class Search {
     }
     catch (IOException e) {
       System.out.println("Reading URL Error: " + e.getMessage());
+      System.exit(2);
     }
 
     newSearch.getLinksAndBodyText(html);
-    newSearch.printResult();
+    newSearch.sortKeywordIndex();
+    newSearch.sortWordFrequencyIndex();
+    newSearch.printResult(printNumber);
   }
 }
